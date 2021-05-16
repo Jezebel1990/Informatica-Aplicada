19a76910036fd97b9f7ebe6fc91eeb9fe5e4a80a
Jezebel1990 <75287031+Jezebel1990@users.noreply.github.com>
2021-05-16 01:15:34 -0300

    Windows Forms

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbN1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbN2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbN3 = new System.Windows.Forms.TextBox();
+            this.txbN4 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbSoma = new System.Windows.Forms.TextBox();
+            this.txbMedia = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero1";
+            // 
+            // txbN1
+            // 
+            this.txbN1.Location = new System.Drawing.Point(78, 59);
+            this.txbN1.Name = "txbN1";
+            this.txbN1.Size = new System.Drawing.Size(64, 20);
+            this.txbN1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero2";
+            // 
+            // txbN2
+            // 
+            this.txbN2.Location = new System.Drawing.Point(179, 59);
+            this.txbN2.Name = "txbN2";
+            this.txbN2.Size = new System.Drawing.Size(61, 20);
+            this.txbN2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numero3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Numero4";
+            // 
+            // txbN3
+            // 
+            this.txbN3.Location = new System.Drawing.Point(279, 59);
+            this.txbN3.Name = "txbN3";
+            this.txbN3.Size = new System.Drawing.Size(64, 20);
+            this.txbN3.TabIndex = 6;
+            this.txbN3.TextChanged += new System.EventHandler(this.txbN3_TextChanged);
+            // 
+            // txbN4
+            // 
+            this.txbN4.Location = new System.Drawing.Point(388, 59);
+            this.txbN4.Name = "txbN4";
+            this.txbN4.Size = new System.Drawing.Size(69, 20);
+            this.txbN4.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(78, 134);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(98, 28);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(211, 134);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(101, 27);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Somatória";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(276, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Média";
+            // 
+            // txbSoma
+            // 
+            this.txbSoma.Location = new System.Drawing.Point(79, 219);
+            this.txbSoma.Name = "txbSoma";
+            this.txbSoma.Size = new System.Drawing.Size(98, 20);
+            this.txbSoma.TabIndex = 14;
+            // 
+            // txbMedia
+            // 
+            this.txbMedia.Location = new System.Drawing.Point(265, 224);
+            this.txbMedia.Name = "txbMedia";
+            this.txbMedia.Size = new System.Drawing.Size(104, 20);
+            this.txbMedia.TabIndex = 15;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(338, 133);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(86, 28);
+            this.btnFechar.TabIndex = 16;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(513, 274);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.txbMedia);
+            this.Controls.Add(this.txbSoma);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txbN4);
+            this.Controls.Add(this.txbN3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbN2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbN1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbN1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbN2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbN3;
+        private System.Windows.Forms.TextBox txbN4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbSoma;
+        private System.Windows.Forms.TextBox txbMedia;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double N1,N2,N3, N4, Soma, Media;
+
+        private void txbN3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txbN1.Text = null;
+            txbN2.Text = null;
+            txbN3.Text = null;
+            txbN4.Text = null;
+            txbSoma.Text = null;
+            txbMedia.Text = null;
+            txbN1.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            N1 = Convert.ToDouble(txbN1.Text.Trim());
+            N2 = Convert.ToDouble(txbN2.Text.Trim());
+            N3 = Convert.ToDouble(txbN3.Text.Trim());
+            N4 = Convert.ToDouble(txbN4.Text.Trim());
+            Soma = N1 + N2 + N3 + N4;
+            Media = Soma / 4;
+            txbSoma.Text = Soma.ToString();
+            txbMedia.Text = Media.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
